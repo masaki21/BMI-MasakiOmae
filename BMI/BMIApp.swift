4ab8d24a56f7503d25c8f76b78cc3d7bf24d192d
masaki21 <omaemasaki21@kit.edu.kh>
2025-11-02 17:11:03 +0900

    Initial Commit

---
+++ BMI/BMIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BMIApp.swift
+//  BMI
+//
+//  Created by MAC on 2/11/25.
+//
+
+import SwiftUI
+
+@main
+struct BMIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BMI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BMI
+//
+//  Created by MAC on 2/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
